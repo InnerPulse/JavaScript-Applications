8da437748a3bacd54f32d2188a4fe8bb0f1ddc92
InnerPulse <stoev95pd@gmail.com>
2021-02-28 18:27:04 +0200

    Solutions added

--- 02 Lab - Asynchronous Programming/01. XHR/scripts/app.js
+++ 02 Lab - Asynchronous Programming/01. XHR/scripts/app.js
@@ -1,3 +1,16 @@
+// Disclaimer:
+// The task is to make XHR (XmlHttpRequest) for educational purposes only!
+// I'm very aware that nowadays we don't use XHR on the majority of websites.
+// For future tasks I will use async/await and not so often then/catch.
+
 function loadRepos() {
-   console.log("TODO...");
+  const url = 'https://api.github.com/users/testnakov/repos';
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.addEventListener('readystatechange', () => {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+      document.getElementById('res').textContent = httpRequest.responseText;
+    }
+  });
+  httpRequest.open('GET', url);
+  httpRequest.send();
 }
