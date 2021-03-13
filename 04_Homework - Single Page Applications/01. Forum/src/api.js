05e2b75882b6c56721261fc51f561ac8b477f0aa
InnerPulse <stoev95pd@gmail.com>
2021-03-14 00:16:31 +0200

    Add Forum app

---
+++ 04_Homework - Single Page Applications/01. Forum/src/api.js
@@ -0,0 +1,16 @@
+export async function request(url, options) {
+    try {
+        const response = await fetch(url, options);
+
+        if (response.ok != true) {
+            const error = await response.json();
+            alert(error.message);
+            throw new Error(error.message);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return alert(error);
+    }
+}
